8ad9fb3861a02ba0acd84bff012adfc56ef1164e
Akira345 <g.akira345@gmail.com>
2020-05-02 13:52:47 +0900

    難易度表示コンポーネント作成

--- src/components/GameControler.js
+++ src/components/GameControler.js
@@ -1,38 +1,40 @@
 import React, { useState } from 'react';
 
+// コントローラ表示
+const Controller = ( props ) => {
+
+  // ラジオボタンのチェックボックスを変更できるようにする
+  // こうしないとReactではイベントをトラップされているので変更できなくなる
+  const [ difficult, setDifficult ] = useState( 'Normal' );
+  const handleOnChange = ( event ) => {
+    setDifficult( event.target.value );
+  };
+  console.log( props.isComplate );
+  if ( props.isComplate ) {
+    return (
+      <>
+        <input type="radio" name="difficult" value="Easy" onChange={ handleOnChange } checked={ difficult === 'Easy' } />やさしい
+        <input type="radio" name="difficult" value="Normal" onChange={ handleOnChange } checked={ difficult === 'Normal' } /> 普通
+        <input type="radio" name="difficult" value="Hard" onChange={ handleOnChange } checked={ difficult === 'Hard' } /> 難しい
+        <br />
+        <input type="submit" name="submit" value="スタート" />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <input type="submit" name="submit" value="ギブアップ" />
+      </>
+    );
+  }
+};
+
 // ゲームコントローラコンポーネント
 const GameController = ( props ) => {
-  // コントローラ表示
-  const Controller = ( isComplate ) => {
-    // ラジオボタンのチェックボックスを変更できるようにする
-    // こうしないとReactではイベントをトラップされているので変更できなくなる
-    const [ difficult, setDifficult ] = useState( 'Normal' );
-    const handleOnChange = ( event ) => {
-      setDifficult( event.target.value );
-    };
-    if ( isComplate ) {
-      return (
-        <>
-          <input type="radio" name="difficult" value="Easy" onChange={ handleOnChange } checked={ difficult === 'Easy' } />やさしい
-          <input type="radio" name="difficult" value="Normal" onChange={ handleOnChange } checked={ difficult === 'Normal' } /> 普通
-          <input type="radio" name="difficult" value="Hard" onChange={ handleOnChange } checked={ difficult === 'Hard' } /> 難しい
-          <br />
-          <input type="submit" name="submit" value="スタート" />
-        </>
-      );
-    } else {
-      return (
-        <>
-          <input type="submit" name="submit" value="ギブアップ" />
-        </>
-      );
-    }
-  };
-
   return (
     <form onSubmit={ props.handleSubmit }>
       <Controller
-        state={ props.state }
+        isComplate={ props.isComplate }
       />
     </form>
   );

1dc5fce70dd1ec457db6582d7d6a4196d02f4e55
Akira345 <g.akira345@gmail.com>
2020-05-03 13:43:01 +0900

    TypeScript準備

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "moduleFileExtensions": [
+    "ts",
+    "js",
+    "tsx",
+    "jsx"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx|jsx|js)$": "ts-jest"
+  },
+  "globals": {
+    "ts-jest": {
+      "tsConfig": "tsconfig.json"
+    }
+  },
+  "testMatch": [
+    "**/tests/**/*.(spec|test).+(ts|tsx|js)"
+  ],
+  "moduleNameMapper": {
+    "^components/(.+)": "<rootDir>/src/components/$1",
+    "\\.(css|scss)$": "<rootDir>/node_modules/jest-css-modules"
+  },
+};
